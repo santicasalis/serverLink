a917f09ac4c1e564e94b9deb61c272d77884174d
santicasalis <santicasalis@hotmail.com>
2023-12-01 14:04:03 -0300

    ch

--- server/src/controllers/nodemailer/welcomeRegister.js
+++ server/src/controllers/nodemailer/welcomeRegister.js
@@ -1,15 +1,15 @@
-require("dotenv").config()
-const {NODEMAILER_USER} = process.env
-const transporter = require("../../../config/nodemailer")
-const welcome = require("../../utils/nodemailer/welcome")
+// require("dotenv").config()
+// const {NODEMAILER_USER} = process.env
+// const transporter = require("../../../config/nodemailer")
+// const welcome = require("../../utils/nodemailer/welcome")
 
-const welcomeRegister = async (name, email) => {
-    await transporter.sendMail({
-        from: NODEMAILER_USER,
-        to: email,
-        subject:"Welcome to ConnectInk",
-        html: `${welcome(name)}`
-    })
-}
+// const welcomeRegister = async (name, email) => {
+//     await transporter.sendMail({
+//         from: NODEMAILER_USER,
+//         to: email,
+//         subject:"Welcome to ConnectInk",
+//         html: `${welcome(name)}`
+//     })
+// }
 
-module.exports = welcomeRegister
+// module.exports = welcomeRegister
